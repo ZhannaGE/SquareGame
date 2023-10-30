5a2edd868504d82cb1063fc76dd826163f843cce
Zhanna Yeloshenko <eloshenko.zhanna@gmail.com>
2023-10-30 11:15:14 +0300

    add sounds, rename url

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-const colors = ['../img/1.png', '../img/2.png', '../img/3.png', '../img/4.png', '../img/5.png', '../img/6.png', '../img/7.png', '../img/8.png', '../img/9.png', '../img/10.png', '../img/11.png', '../img/12.png', '../img/13.png', '../img/14.png', '../img/15.png', '../img/16.png', '../img/17.png', '../img/18.png', '../img/18.png', '../img/20.png', '../img/21.png', '../img/22.png', '../img/23.png', '../img/25.png', '../img/25.png', '../img/26.png', '../img/27.png', '../img/28.png', '../img/29.png'];
+
+const colors = ['./img/1.png', './img/2.png', './img/3.png', './img/4.png', './img/5.png', './img/6.png', './img/7.png', './img/8.png', './img/9.png', './img/10.png', './img/11.png', './img/12.png', './img/13.png', './img/14.png', './img/15.png', './img/16.png', './img/17.png', './img/18.png', './img/18.png', './img/20.png', './img/21.png', './img/22.png', './img/23.png', './img/25.png', './img/25.png', './img/26.png', './img/27.png', './img/28.png', './img/29.png'];
 
 //Функция для генерации случайного числа в заданном диапазоне
 function getRandomInt(min, max) {
@@ -304,23 +305,23 @@ class SquareGame {
 
 
 
-// Функция для воспроизведения звука при клике
-// function music() {
-//     const squareWrapper = document.querySelector('.squareWrapper');
-//     const buttonStart = document.querySelector('.start-button');
-//     const audioSquare = new Audio('../audio/click.ogg');
-//     const audioButtonStart = new Audio('../audio/songscard.mp3');
-//     function playClickSound(song) {
-//         song.currentTime = 0;
-//         song.play();
-//     }
-//
-//     squareWrapper.addEventListener('click', () => playClickSound(audioSquare));
-//     buttonStart.addEventListener('click', () => playClickSound(audioButtonStart));
-//
-// }
-//
-// music()
+//Функция для воспроизведения звука при клике
+function music() {
+    const squareWrapper = document.querySelector('.squareWrapper');
+    const buttonStart = document.querySelector('.start-button');
+    const audioSquare = new Audio('../audio/click.ogg');
+    const audioButtonStart = new Audio('../audio/songscard.mp3');
+    function playClickSound(song) {
+        song.currentTime = 0;
+        song.play();
+    }
+
+    squareWrapper.addEventListener('click', () => playClickSound(audioSquare));
+    buttonStart.addEventListener('click', () => playClickSound(audioButtonStart));
+
+}
+
+music()
 
 let squareGame = new SquareGame(1, ".simpleGameApp");
 squareGame.start();
